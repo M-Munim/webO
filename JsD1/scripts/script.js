26478d85313e387a6dcb7bab20fd1d4bc38d2b86
M-Munim <muhammadmunimoff330@gmail.com>
2025-08-02 18:51:54 +0500

    js day 1

---
+++ JsD1/scripts/script.js
@@ -0,0 +1,63 @@
+var myName = "Munim";
+var myName = "Muhammad Munim"  //✅
+myName = "ali"
+console.log(myName);
+
+let uniName = "Daffodil International University";
+// let uniName = "Daffodil International University"; ❌
+uniName = "FAST"
+console.log(uniName);
+
+const pi = 3.14;
+// const pi = 3.14;❌
+// pi = 3.15 ❌
+console.log(pi);
+
+//--------------------------------------------------------
+
+let myNamee = "Munim";
+let myAge = 23;
+let isTrainer = true;
+let x;
+let y = null;
+
+
+const myData = {
+    name: "Munim",
+    age: 23,
+    isTrainer: true,
+    address: {
+        city: "Islamabad",
+        country: "Pakistan"
+    }
+}
+
+
+console.log(typeof myData)
+
+const languages = ["html", "css", "js", "php", "sql", "c++", "c#"]
+
+const serialNo = [22, 3434, 3452354, 25432, 542345]
+
+
+function add() {
+
+}
+
+
+
+
+const hisName = "Shahwez"
+console.log(typeof hisName)
+
+const hisAge = 21
+console.log(typeof hisAge)
+
+const hisTrainer = false
+console.log(typeof hisTrainer)
+
+
+
+
+document.writeln("Hello World")
+document.write(2 + 2)
