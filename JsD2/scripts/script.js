f6965dc66001a602dee3111a598ef2aea5dadbaf
M-Munim <muhammadmunimoff330@gmail.com>
2025-08-03 18:30:21 +0500

    operators, if , ifelse, ifelse ladder

---
+++ JsD2/scripts/script.js
@@ -0,0 +1,84 @@
+let a = 10;
+let b = 20;
+console.log(a + b) //30
+console.log(a - b) // -10
+console.log(a * b) // 200
+console.log(a / b) // 0.5
+console.log(a % b) //10
+console.log(a ** b) // 100000000000000000000000
+
+
+let srNo = 1;
+console.log(++srNo);
+console.log(srNo++);
+
+
+let srNo1 = 1;
+console.log(srNo1--);
+console.log(srNo1);
+
+
+
+let amount = 100;
+console.log(amount += 10);
+
+
+let pay = 300;
+console.log(pay -= 50);
+
+let cars = 2;
+console.log(cars *= 2);
+
+
+
+console.log(2 == 2)
+console.log('4' == 4)
+console.log('4' === 4)
+console.log('4' != 4)
+console.log('4' !== 4)
+console.log(10 <= 20)
+
+console.log(true && false)
+console.log(true || false)
+console.log(!false)
+console.log(!true)
+
+
+let fName = "Muhammad"
+let lName = "Munim"
+
+console.log(fName + " " + lName)
+
+let result = 5 + 3 * 2;
+let result2 = (5 + 3) * 2;
+console.log(result);
+console.log(result2);
+
+let myAge = 13;
+
+if (myAge > 20) {
+    console.log("You are eligible to vote")
+} else {
+    console.log("You are not eligible to vote")
+}
+
+
+let name = "ali"
+if (name === "Munim") {
+    console.log("Hello sir ")
+} else {
+    console.log("Hello guest")
+}
+
+
+let grade = "a";
+
+if (grade === "A") {
+    console.log("Excellent")
+} else if (grade === "B") {
+    console.log("Good")
+} else if (grade === "C") {
+    console.log("Average")
+} else {
+    console.log("Fail")
+}
